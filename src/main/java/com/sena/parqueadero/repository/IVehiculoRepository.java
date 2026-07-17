0153fed1f9f157291c16c7c032c448d095538272
THE-LEVITE <halfonsokyar03@gmail.com>
2026-07-16 22:14:27 -0500

    vo.3_interfacePaqueteRepository_alevil_16062026

---
+++ src/main/java/com/sena/parqueadero/repository/IVehiculoRepository.java
@@ -0,0 +1,23 @@
+package com.sena.parqueadero.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import com.sena.parqueadero.model.Vehiculo;
+
+import jakarta.persistence.LockModeType;
+
+public interface IVehiculoRepository extends JpaRepository<Vehiculo, Long> {
+
+	Optional<Vehiculo> findByPlaca(String Placa);
+
+	boolean existsByPlaca(String Placa);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT v FROM Vehiculo v WHERE v.placa = : placa")
+	Optional<Vehiculo> findPlacaForUpdate(String placa);
+
+}
